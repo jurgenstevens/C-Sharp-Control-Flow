97dd0836ccfea32fa9f1921a47ee8f13ab6b8f5f
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-08 23:20:51 -0600

    Return statements with cube method.

--- Program.cs
+++ Program.cs
@@ -9,21 +9,16 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
-            // ARRAYS
-            // When creating an array in C#, include the brackets to let it know you're doing so
-            // that's right, you also use curly braces as to contain the data
-            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
-            Console.WriteLine(luckyNumbers[4]);
-            // luckyNumbers[1] = 9;
-            // Console.WriteLine(luckyNumbers[1]);
-
-            // when creating a new array, include its data type and how many elements the array can hold
-            string[] friends = new string[5];
-            friends[0] = "Dzenita";
-            friends[1] = "David";
-            friends[2] = "Chris";
-
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
             Console.ReadLine();
         }
+        // specify the data type you're going to return with the method
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+
     }
 }
