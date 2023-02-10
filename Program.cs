5539ada30b06315cdea3977689a648b371e9add8
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-09 19:30:38 -0600

    Complete while loops section.

--- Program.cs
+++ Program.cs
@@ -9,26 +9,23 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Max(25, 12, 20));
+            // WHILE LOOPS
+            int index = 1;
+            // WHILE LOOP
+            //while (index <= 5)
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+            //}
+            // DO WHILE LOOP 
+            //do
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+            //} while (index <= 5);
 
             Console.ReadLine();
         }
-        // call this method a static int since the result will be an integer
-        static int Max(int num1, int num2, int num3)
-        {
-            int result;
-            if(num1 >= num2 && num1 >= num3)
-            {
-                result = num1;
-            } else if (num2 >= num1 && num2 >= num3)
-            {
-                result = num2;
-            } else
-            {
-                result = num3;
-            }
 
-            return result;
-        }
     }
 }
