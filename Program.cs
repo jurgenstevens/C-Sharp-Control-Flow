bdde1e0dc543e4108b539e69fbedceff10f5dd1f
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-09 19:44:44 -0600

    Complete while loop guessing games.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ControlFlow
 {
@@ -11,21 +12,37 @@ namespace ControlFlow
         {
             // WHILE LOOPS
             int index = 1;
-            // WHILE LOOP
-            //while (index <= 5)
-            //{
-            //    Console.WriteLine(index);
-            //    index++;
-            //}
-            // DO WHILE LOOP 
-            //do
-            //{
-            //    Console.WriteLine(index);
-            //    index++;
-            //} while (index <= 5);
+            WHILE LOOP
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            DO WHILE LOOP
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
+            // WHILE LOOP GAME
+            string secretWord = "giraffe";
+            string guess = "";
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            }
+            Console.Write("You win!");
+            // DO WHILE GAME
+            do
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            } while (guess != secretWord);
+            Console.Write("You win!");
 
             Console.ReadLine();
         }
-
     }
 }
